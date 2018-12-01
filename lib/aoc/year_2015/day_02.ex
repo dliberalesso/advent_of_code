10b29f0fc72c802c503383b0df5c8d962fd7138f
Douglas Liberalesso <dliberalesso@gmail.com>
2018-12-01 21:54:25 -0200

    Day 02 of 2015

---
+++ lib/aoc/year_2015/day_02.ex
@@ -0,0 +1,105 @@
+defmodule Aoc.Year2015.Day02 do
+  @moduledoc """
+  Solution to Day 02 of 2015: I Was Told There Would Be No Math
+
+  ## --- Day 02: I Was Told There Would Be No Math ---
+
+  The elves are running low on wrapping paper, and so they need to submit an order
+  for more. They have a list of the dimensions (length `l`, width `w`, and height
+  `h`) of each present, and only want to order exactly as much as they need.
+
+  Fortunately, every present is a box (a perfect right rectangular prism), which
+  makes calculating the required wrapping paper for each gift a little easier:
+  find the surface area of the box, which is `2*l*w + 2*w*h + 2*h*l`. The elves
+  also need a little extra paper for each present: the area of the smallest side.
+
+  For example:
+
+  - A present with dimensions `2x3x4` requires `2*6 + 2*12 + 2*8 = 52` square feet of wrapping paper plus `6` square feet of slack, for a total of `58` square feet.
+  - A present with dimensions `1x1x10` requires `2*1 + 2*10 + 2*10 = 42` square feet of wrapping paper plus `1` square foot of slack, for a total of `43` square feet.
+
+  All numbers in the elves' list are in feet. How many total *square feet of
+  wrapping paper* should they order?
+
+  ## --- Part Two ---
+
+  The elves are also running low on ribbon. Ribbon is all the same width, so they
+  only have to worry about the length they need to order, which they would again
+  like to be exact.
+
+  The ribbon required to wrap a present is the shortest distance around its sides,
+  or the smallest perimeter of any one face. Each present also requires a bow made
+  out of ribbon as well; the feet of ribbon required for the perfect bow is equal
+  to the cubic feet of volume of the present. Don't ask how they tie the bow,
+  though; they'll never tell.
+
+  For example:
+
+  - A present with dimensions `2x3x4` requires `2+2+3+3 = 10` feet of ribbon to wrap the present plus `2*3*4 = 24` feet of ribbon for the bow, for a total of `34` feet.
+  - A present with dimensions `1x1x10` requires `1+1+1+1 = 4` feet of ribbon to wrap the present plus `1*1*10 = 10` feet of ribbon for the bow, for a total of `14` feet.
+
+  How many total *feet of ribbon* should they order?
+  """
+
+  @doc """
+
+  """
+  def part_1(input) do
+    input
+    |> split_into_lists_of_ints()
+    |> Enum.reduce(0, fn x, acc -> sqft_of_paper_per_box(x) + acc end)
+  end
+
+  defp sqft_of_paper_per_box(list_of_ints) do
+    list_of_ints
+    |> calculate_areas
+    |> surface_area_and_slack
+  end
+
+  defp calculate_areas([l, w, h]), do: [l * w, w * h, h * l]
+
+  defp surface_area_and_slack(list) do
+    Enum.reduce(
+      list, # list of areas
+      Enum.min(list), # smallest side will be the initial value of acc
+      fn x, acc -> x + x + acc end
+    )
+  end
+
+  @doc """
+
+  """
+  def part_2(input) do
+    input
+    |> split_into_lists_of_ints()
+    |> Enum.reduce(0, fn x, acc -> total_feet_of_ribbon_per_box(x) + acc end)
+  end
+
+  defp total_feet_of_ribbon_per_box(list) do
+    Enum.reduce(
+      remove_max(list),
+      feet_of_ribbon_to_bow(list),
+      fn x, acc -> x + x + acc end
+    )
+  end
+
+  defp remove_max(list) do
+    list
+    |> Enum.sort
+    |> Enum.drop(-1)
+  end
+
+  defp feet_of_ribbon_to_bow([l, w, h]), do: l * w * h
+
+  defp split_into_lists_of_ints(string) do
+    string
+    |> String.split()
+    |> Enum.map(&string_to_integer_list/1)
+  end
+
+  defp string_to_integer_list(string) do
+    string
+    |> String.split("x")
+    |> Enum.map(&String.to_integer/1)
+  end
+end
---
+++ test/aoc/year_2015/day_02_test.exs
@@ -0,0 +1,30 @@
+defmodule Aoc.Year2015.Day02Test do
+  use Aoc.DayCase
+  doctest Aoc.Year2015.Day02, import: true
+
+  alias Aoc.Year2015.Day02
+
+  describe "part_1/1" do
+    test "examples" do
+      assert Day02.part_1("2x3x4") == 58
+      assert Day02.part_1("1x1x10") == 43
+    end
+
+    @tag day: 02, year: 2015
+    test "input", %{input: input} do
+      assert input |> Day02.part_1() == 1588178
+    end
+  end
+
+  describe "part_2/1" do
+    test "examples" do
+      assert Day02.part_2("2x3x4") == 34
+      assert Day02.part_2("1x1x10") == 14
+    end
+
+    @tag day: 02, year: 2015
+    test "input", %{input: input} do
+      assert input |> Day02.part_2() == 3783758
+    end
+  end
+end
